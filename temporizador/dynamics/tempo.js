1ae206dc3e04612c25fa784230d9e80e24696137
unknown <321129181@alumno.enp.unam.mx.com>
2022-05-26 02:33:59 -0500

    arreglado bug del contador

--- temporizador/dynamics/tempo.js
+++ temporizador/dynamics/tempo.js
@@ -26,36 +26,51 @@ iniciar.addEventListener("click", ()=>{
     min = minutos.value;
     seg = segundos.value;
     
-      let tiempos = setInterval(() => {
-        tiempo.innerHTML = min + ":" + seg;  
-        console.log(min + ":" + seg);
-        
-        if(seg > 0 && min > 0){
-          if(seg <= 59 && seg > 0){
-            seg--;
-          }else{
-            seg=59;
-            min--;
-          }
-        }else{
-          clearInterval(tiempos);
+    var tiempos = setInterval(() => {
+      if(min < 10 && seg < 10){
+        tiempo.innerHTML = 0 + min + ":" + 0 + seg;  
+      }
+      if(min < 10 && seg >= 10){
+        tiempo.innerHTML = 0 + min + ":" + seg;  
+      }
+      if(min >= 10 && seg < 10){
+        tiempo.innerHTML = min + ":" + seg;
+      } 
+      console.log(min + ":" + seg);
+      
+      if(min >= 0){
+        if(seg <= 59 && seg > 0){
+          seg--;
+        }else if(min > 0){
+          seg=59;
+          min--;
         }
-      }, 1000);
+      }
+    }, 1000);
     
     var tempo = setTimeout(()=>{
       alarma=true;
       console.log("si"+alarma);
       if(alarma == true){
+        min=0;
+        seg=0;
+        clearInterval(tiempos);
         alarmita.volume=0.5;
         alarmita.play();
+        tiempo.innerHTML = 0 + min + ":" + 0 + seg;
       }
-    }, (minutos.value * 60000) + (segundos.value * 1000))
+    }, (minutos.value * 60000) + (segundos.value * 1000)+1000)
+    
   }
   iniciar.addEventListener("click", ()=>{
     clearTimeout(tempo);
   })
   parar.addEventListener("click", ()=>{
     clearTimeout(tempo);
+    clearInterval(tiempos);
+    min=0;
+    seg=0;
+    tiempo.innerHTML = 0 + min + ":" + 0 + seg;
   })
 })
 
